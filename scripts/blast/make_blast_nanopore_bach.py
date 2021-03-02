24c7c9012189ec9b4df3cd76ebc2460503b9fbbb
19971112 <warashinatomorrow1112@gmail.com>
2021-03-02 21:29:53 +0900

    Update make_blast_nanopore_bach.py

--- scripts/blast/make_blast_nanopore_bach.py
+++ scripts/blast/make_blast_nanopore_bach.py
@@ -7,21 +7,31 @@ def main():
   work_path = sys.argv[2]
   dirs_path_list = glob.glob(data_path)
   
-  script1 = open('~/github/NPP/scripts/nanopore_16S_1.sh', 'r')
+  scr1 = '~/github/NPP/scripts/nanopore_16S_1.sh'
+  scr2 = '~/github/NPP/scripts/nanopore_16S_2.sh'
   
   for each_dir_path in dirs_path_list:
-    dir_basename = os.path.basename(each_dir_path)
-    file_name = 'nanopore_16S_1_' + dir_basename + '.job'
-    dir_path = data_path + data_basename
-    custom_script = original_script1.replace("$working_dir", work_path)
-    custom_script = custom_script.replace("$data_dir", dir_path)
-    
-    make_script_file(file_name,custom_script)
+    file_name = return_file_name(each_dir_path,1)
+    custom_script = return_custum_script(scr1,work_path)
+    make_script_file(file_name,custom_script,each_dir_path)
 
     
 def make_script_file(FILE_NAME,SCRIPT_TXT):
   file = opne(FILE_NAME, 'w')
   file.write(SCRIPT_TXT)
+
   
+def return_file_name(DIR_PATH, NUMBER):
+  dir_basename = os.path.basename(DIR_PATH)
+  file_name = 'nanopore_16S_' + NUMBER + '_' + dir_basename + '.job'
+  return file_name
+
+
+def return_custum_script(SCRIPT_PATH,WORK_PATH,EACH_DIR_PATH):
+  output = open(SCRIPT_PATH, 'r')
+  output_txt = output.read()
+  output_txt = output_txt.replace("$working_dir", WORK_PATH)
+  output_txt = output_txt.replace("$data_dir", EACH_DIR_PATH)
+  return output_txt
   
 main()
