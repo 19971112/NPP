3b911dcc1db34c5527006d97b23377df64576514
19971112 <warashinatomorrow1112@gmail.com>
2021-03-05 11:51:07 +0900

    Update add-text.py

--- scripts/tools/add-text.py
+++ scripts/tools/add-text.py
@@ -1,7 +1,6 @@
-from PyPDF2 import PdfFileReader
-
+import random
 import io
-
+from PyPDF2 import PdfFileReader
 from PyPDF2 import PdfFileWriter
 from reportlab.lib.colors import Color
 from reportlab.lib.pagesizes import A4
@@ -11,8 +10,6 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from natsort import natsorted
 
-import random
-import glob
 
 pdfmetrics.registerFont(
     TTFont(
@@ -21,7 +18,8 @@ pdfmetrics.registerFont(
     )
 )
 
-files = PdfFileReader("./word_sample.pdf")
+file = "./word_sample.pdf"
+pdf = PdfFileReader(file)
 
 box = ["B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11","B12","B13","B14","B15","B16","B17","B18","B19","B20","B21","B22","B23","B24","B25","B26","M1","M2","M3","M4","M5","M6","M7","M8","M9","M10","M11","M12","M13","M14","M15","M16","M17","M18","M19","M20","M21","M22","M23","MB1","MB2","MB3","MB4","MB5","MB6","MB7","MB8","MB9","MB10","MB11","MB12","MB13","MB14","MB15"]
 box = ["B1"]
@@ -31,7 +29,7 @@ page_count = 0
 
 for each_page in box:
   
-    pdf = PdfFileReader(files)
+    
     page = pdf.getPage(page_count)
     
     input_txt = box[page_count]
@@ -54,6 +52,6 @@ for each_page in box:
 
     output = PdfFileWriter()
     output.addPage(page)
-    output_file_name = str(page_count) + '_' + each_page
+    output_file_name = str(page_count) + '_' + each_page + '.pdf'
     with open(output_file_name, "wb") as fout:
         output.write(fout)
